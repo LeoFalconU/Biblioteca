2beed327e9b0f176cecf890f092f1a5af1c588d6
SarahSantana1 <20253rd044@utez.edu.mx>
2026-04-16 16:41:57 -0600

    Se agregaron los botones y los espacios de usuario y contraseña

--- src/main/java/Biblioteca/Model/Usuario.java
+++ src/main/java/Biblioteca/Model/Usuario.java
@@ -1,4 +1,10 @@
-package BibliotecaModel;
+package Biblioteca.Model;
 
 public class Usuario {
+    private String user = "admin";
+    private String password = "1234";
+
+    public boolean login(String user, String password){
+        return user.equals(user) && password.equals(password);
+    }
 }
