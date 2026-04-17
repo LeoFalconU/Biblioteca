6b3e76993a52e6ff0ff3061d269eef0222f06c99
AxelLJ57 <20253rd038@utez.edu.mx>
2026-04-17 16:20:11 -0600

    se nombraron los textfield y label

--- src/main/java/com/example/biblioteca/controller/AgregarLibroController.java
+++ src/main/java/com/example/biblioteca/controller/AgregarLibroController.java
@@ -2,13 +2,25 @@ package com.example.biblioteca.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class AgregarLibroController {
-    @FXML
-    private Label welcomeText;
+    @FXML private TextField    txtNumero;
+    @FXML private TextField    txtTitulo;
+    @FXML private TextField    txtAutor;
+    @FXML private Label lblMensaje;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
 }
