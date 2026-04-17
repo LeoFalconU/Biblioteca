d219dd5b4955ea53bd16c6d2c6c9cc89a0cd55a2
AxelLJ57 <20253rd038@utez.edu.mx>
2026-04-17 16:08:29 -0600

    Se agregaron los atributos y el constructor vacio y con datos

--- src/main/java/com/example/biblioteca/model/DatosLibro.java
+++ src/main/java/com/example/biblioteca/model/DatosLibro.java
@@ -1,5 +1,21 @@
-package com.example.biblioteca.model;
+package com.example.biblioteca.Model;
 
 public class DatosLibro {
-    //agregar getters y setter para los datos de libro, clase padre, contructores vecio y con info
+    private int numero;
+    private String titulo;
+    private String autor;
+    private String genero;
+
+
+    public DatosLibro() {
+    }
+
+
+    public DatosLibro(int numero, String titulo, String autor, String genero) {
+        this.numero = numero;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+        //agregar getters y setter para los datos de libro, clase padre, contructores vecio y con info
+    }
 }
