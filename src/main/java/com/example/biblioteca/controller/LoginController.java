66c5f140fc53397f103d4a0931b85b283a50fd8d
LeoFalconU <20253rd053@utez.edu.mx>
2026-04-17 16:14:37 -0600

    "Se agergaron metodos y variables para el menu"

--- src/main/java/com/example/biblioteca/controller/LoginController.java
+++ src/main/java/com/example/biblioteca/controller/LoginController.java
@@ -1,4 +1,49 @@
 package com.example.biblioteca.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 public class LoginController {
+}
+
+@FXML private StackPane contenedor;
+
+
+@FXML
+private void irAgregarLibro() {
+    cargarVista("AgregarLibro.fxml");
+}
+
+
+@FXML
+private void irBuscarLibro() {
+    cargarVista("BuscarLibro.fxml");
+}
+
+
+@FXML
+private void cerrarSesion() {
+    try {
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Stage stage = (Stage) contenedor.getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Biblioteca - Login");
+        stage.show();
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
+
+private void cargarVista(String fxml) {
+    try {
+        Parent vista = FXMLLoader.load(getClass().getResource(fxml));
+        contenedor.getChildren().setAll(vista);
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
 }
