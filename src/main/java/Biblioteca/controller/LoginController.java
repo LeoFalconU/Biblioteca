04524d71a4a03c4fb0d3093ba57e93783019dfb1
SarahSantana1 <20253rd044@utez.edu.mx>
2026-04-17 15:36:41 -0600

    SeIniciar aplicación en pantalla de login

--- src/main/java/Biblioteca/controller/LoginController.java
+++ src/main/java/Biblioteca/controller/LoginController.java
@@ -1,45 +1,43 @@
-package Controller;
+package Biblioteca.controller;
 
-import Biblioteca.Model.Usuario;
+import Biblioteca.modelo.Usuario;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import javafx.scene.control.Control;
+import javafx.scene.control.*;
 
 public class LoginController {
-    @FXML
-    private TextField txtUser;
-    @FXML
-    private PasswordField txtPassword;
+
+    @FXML private TextField txtUser;
+    @FXML private PasswordField txtPass;
 
     private Usuario modelo = new Usuario();
 
     @FXML
-    private void login(){
+    private void login() {
         try {
             String user = txtUser.getText();
-            String password = txtPassword.getText();
+            String pass = txtPass.getText();
 
-            if (modelo.login(user, password)) {
+            if (modelo.login(user, pass)) {
+
                 Stage stage = (Stage) txtUser.getScene().getWindow();
 
                 FXMLLoader loader = new FXMLLoader(
-                        getClass().getResource("/biblioteca/vista/vista.fxml"));
+                        getClass().getResource("/biblioteca/vista/login.fxml")
+                );
 
                 stage.setScene(new Scene(loader.load()));
-                stage.setTitle("Biblioteca");
-            }else {
+
+            } else {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setContentText("Usuario o contraseña incorrectos");
                 alert.show();
             }
-        }
- catch (Exception e) {
-        e.printStackTrace();
-    }
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/main/java/Biblioteca/main/Main.java
@@ -0,0 +1,27 @@
+package Biblioteca.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/biblioteca/vista/login.fxml")
+        );
+
+        Scene scene = new Scene(loader.load());
+
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
