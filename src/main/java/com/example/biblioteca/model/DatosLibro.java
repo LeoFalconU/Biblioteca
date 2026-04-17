c59ba19573bd202615e22e989da7b90510b6b191
AxelLJ57 <20253rd038@utez.edu.mx>
2026-04-17 16:11:59 -0600

    se creo el metodo toString para representar el objeto como texto

--- src/main/java/com/example/biblioteca/model/DatosLibro.java
+++ src/main/java/com/example/biblioteca/model/DatosLibro.java
@@ -17,13 +17,34 @@ public class DatosLibro {
         this.autor = autor;
         this.genero = genero;
     }
-    public int    getNumero() { return numero; }
-    public String getTitulo() { return titulo; }
-    public String getAutor()  { return autor;  }
-    public String getGenero() { return genero; }
+    //Getters
+    public int    getNumero() {
+        return numero; }
+    public String getTitulo() {
+        return titulo; }
+    public String getAutor()  {
+        return autor;  }
+    public String getGenero() {
+        return genero; }
 
-    public void setNumero(int    numero) { this.numero = numero; }
-    public void setTitulo(String titulo) { this.titulo = titulo; }
-    public void setAutor (String autor)  { this.autor  = autor;  }
-    public void setGenero(String genero) { this.genero = genero; }
+    //Setters
+    public void setNumero(int    numero) {
+        this.numero = numero; }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo; }
+    public void setAutor (String autor)  {
+        this.autor  = autor;  }
+    public void setGenero(String genero) {
+        this.genero = genero; }
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "numero="  + numero  +
+                ", titulo='" + titulo  + '\'' +
+                ", autor='"  + autor   + '\'' +
+                ", genero='" + genero  + '\'' +
+                '}';
+    }
+
 }
